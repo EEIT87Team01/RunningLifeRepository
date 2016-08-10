fd635ef121bac4b75c6bc7dc541813a9f7ff2ec2
陳柏維 <ethan@gmail.com>
2016-08-10 18:25:48 +0800

    測試JAVA檔

--- RunningLife/src/test/ethan/test.java
+++ RunningLife/src/test/ethan/test.java
@@ -36,6 +36,8 @@ public class test extends HttpServlet {
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doGet(request, response);
+		
+		int aaa;
 	}
 
 }
